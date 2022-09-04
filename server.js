3bc9e33378e2bd9541d1369c318f315f53094ebb
Luke James Yengopal <86246381+chilledoutluke@users.noreply.github.com>
2022-09-04 11:33:42 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
+  }
+});
+
+const app = express();
+
+app.use(cors())
+app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+
+
+app.get('/', (req, res)=> {res.send('it is working!')})
+app.post('/register',(req,res) => { register.handleRegister(req, res, db, bcrypt )})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { Image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { Image.handleApiCall(req, res)})
+
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
+})
